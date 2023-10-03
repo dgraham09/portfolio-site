b6191d33cdb16d1f3f161cdef64a55dad6838ba8
David Graham <davidcgraham09@gmail.com>
2023-10-03 19:07:33 +0200

    Added draft navbar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "../src/components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;

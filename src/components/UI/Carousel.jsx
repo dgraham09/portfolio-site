ade76fcbc50ce30bc775368d7b7abc0c2128085e
David Graham <davidcgraham09@gmail.com>
2023-10-19 18:26:59 +0200

    Added Carousel

--- src/components/UI/Carousel.jsx
+++ src/components/UI/Carousel.jsx
@@ -57,23 +57,23 @@ const CarouselDefault = () => {
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
-        stroke-width="1.5"
+        strokeWidth="1.5"
         stroke="currentColor"
-        class="h-6 w-6">
+        class="h-6 w-6 direction-arrows">
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           d="M15.75 19.5L8.25 12l7.5-7.5" />
       </svg>
     </span>
     <span
-      class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+      class="!absolute !-m-px !h-px !w-px !overflow-hidden text-white !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)] direction-arrows"
       >Previous</span
     >
   </button>
 
   <button
-    class="absolute bottom-0 right-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
+    class="absolute bottom-0 right-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none "
     type="button"
     data-te-target="#carouselExampleControls"
     data-te-slide="next">
@@ -82,13 +82,14 @@ const CarouselDefault = () => {
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
-        stroke-width="1.5"
+        strokeWidth="1.5"
         stroke="currentColor"
-        class="h-6 w-6">
+        class="h-6 w-6 direction-arrows">
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M8.25 4.5l7.5 7.5-7.5 7.5" 
+         />
       </svg>
     </span>
     <span
